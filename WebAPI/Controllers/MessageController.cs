9717b8039fc9b572dd28b3baca09f5abd87c0fbd
Andrej Tomas <andrejtomas@aol.com>
2022-03-15 09:51:30 +0100

    Add project files.

---
+++ WebAPI/Controllers/MessageController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MessageController : Controller
+    {
+
+        [HttpPost(Name = "GetMessageFromExternalApi")]
+        public ActionResult<string> Get(string message)
+        {
+            return message;
+        }
+    }
+}
